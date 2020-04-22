99ab35f030c7747aafe43b219a5fb8aa00e4a523
shaykeshok <shayke@EPR-248-Shayke.es.local>
2020-04-22 22:08:37 +0300

    comments

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -12,28 +12,28 @@ namespace family{
 
     Tree &Tree::addFather(string son_name, string father_name)
     {
-        cout<<"\naddFather"<<"\nson_name: "<<son_name<<"\nfather_name: "<<father_name<<endl;
+        // cout<<"\naddFather"<<"\nson_name: "<<son_name<<"\nfather_name: "<<father_name<<endl;
 
          Node *iterSon=findNode(son_name, this->root);
          if (iterSon==nullptr){
-            cout<<"Exception: "<<"(!findNode(son_name, this->root, iterSon)"<<endl;
+            // cout<<"Exception: "<<"(!findNode(son_name, this->root, iterSon)"<<endl;
             string ex="son not found";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
         }
         // cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
-        cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
+        // cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
 
         //בדיקה שהפויינטר לא ריק
         if (iterSon == nullptr){
-            cout<<"Exception: "<<"iterSon == nullptr"<<endl;
+            // cout<<"Exception: "<<"iterSon == nullptr"<<endl;
             string ex="Must have root to the tree";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
         }
         //בדיקה שאין לו אבא
         if (iterSon->father != nullptr){
-            cout<<"Exception: "<<"iterSon->father != nullptr)"<<son_name<<father_name<<"  "<<iterSon->father->name<<endl;
+            // cout<<"Exception: "<<"iterSon->father != nullptr)"<<son_name<<father_name<<"  "<<iterSon->father->name<<endl;
             string ex="Son have a father";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
@@ -46,20 +46,20 @@ namespace family{
         this->items.push_back(father);
         //הגדלת השלב של העץ
         if(this->level<father->level) this->level=father->level;
-        cout<<"-----------------------------"<<endl;
+        // cout<<"-----------------------------"<<endl;
         return *this;
     };
 
      Node* Tree::findNode(string name, Node *iter){
-        cout<<"\n\nfindNode->"<<name<<endl;
+        // cout<<"\n\nfindNode->"<<name<<endl;
 
         list <Node*> :: iterator it; 
 
         for(it = this->items.begin(); it != this->items.end(); it++){
-            cout<<"(*it)->name: "<<(*it)->name<<endl;
+            // cout<<"(*it)->name: "<<(*it)->name<<endl;
 
             if((*it)->name.compare(name)==0){
-                cout<<"I find node with name: "<<(*it)->name<<endl;
+                // cout<<"I find node with name: "<<(*it)->name<<endl;
                 // iterSon=*it;
                 return *it;
             } 
@@ -69,27 +69,27 @@ namespace family{
 
     Tree &Tree::addMother(string son_name, string mom_name)
     {
-        cout<<"\naddMother"<<"\nson_name: "<<son_name<<"\nmom_name: "<<mom_name<<endl;
+        // cout<<"\naddMother"<<"\nson_name: "<<son_name<<"\nmom_name: "<<mom_name<<endl;
         Node *iterSon=findNode(son_name, this->root);
         if (iterSon==nullptr){
-            cout<<"Exception: "<<"(!findNode(son_name, this->root, iterSon)"<<endl;
+            // cout<<"Exception: "<<"(!findNode(son_name, this->root, iterSon)"<<endl;
             string ex="son not found";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
         }
         // cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
-        cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
+        // cout<<"\nson name after return from findNode: "<<iterSon->name<<endl;
 
         //בדיקה שהפויינטר לא ריק
         if (iterSon == nullptr){
-            cout<<"Exception: "<<"iterSon == nullptr"<<endl;
+            // cout<<"Exception: "<<"iterSon == nullptr"<<endl;
             string ex="Must have root to the tree";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
         }
         //בדיקה שאין לו אמא
         if (iterSon->mother != nullptr){
-            cout<<"Exception: "<<"iterSon->mother != nullptr"<<endl;
+            // cout<<"Exception: "<<"iterSon->mother != nullptr"<<endl;
             string ex="Son have a mother";
             const char * c = ex.c_str();
             __throw_runtime_error(c);
@@ -102,7 +102,7 @@ namespace family{
         this->items.push_back(mother);
         //הגדלת השלב של העץ
         if(this->level<mother->level) this->level=mother->level;
-        cout<<"-----------------------------"<<endl;
+        // cout<<"-----------------------------"<<endl;
         return *this;
     };
 
@@ -128,8 +128,8 @@ void Tree::print_tree_structure(Node* iter, int spaces)
 }
     string Tree::relation(string name){
         //בדיקה שהאיש קיים והחזרה של מצביע עליו
-        cout<<"\n\n\n----------------------------"<<endl;
-        cout<<"relation: "<<name<<endl;
+        // cout<<"\n\n\n----------------------------"<<endl;
+        // cout<<"relation: "<<name<<endl;
         Node *iter = findNode(name, this->root);
         if (iter==nullptr)
            return "unrelated";
@@ -137,7 +137,7 @@ void Tree::print_tree_structure(Node* iter, int spaces)
         {
             int level=iter->level;
             string gender = "";
-            cout<<"name: "<<iter->name<<"\ngender: "<<iter->gender<<endl;
+            // cout<<"name: "<<iter->name<<"\ngender: "<<iter->gender<<endl;
             if (iter->gender == 1)
                 gender = "father";
             else
@@ -168,17 +168,17 @@ void Tree::print_tree_structure(Node* iter, int spaces)
     };
 
     string Tree::find(string kirva){
-        cout<<"\n\n\n----------------------------"<<endl;
-        cout<<"find: "<<kirva<<endl;
+        // cout<<"\n\n\n----------------------------"<<endl;
+        // cout<<"find: "<<kirva<<endl;
 
         //בדיקה שקיבלתי מחרוזת תקינה
          vector<string> v1=split(kirva,false);
             for(int i=0;i<v1.size();i++){ //מעבר על הוקטור של המילים
                 string temp=v1[i];
-                cout<<"temp: "<<temp<<endl;
+                // cout<<"temp: "<<temp<<endl;
                 std::transform(temp.begin(), temp.end(),temp.begin(), ::tolower);
                 if(temp.compare("me")!=0 && temp.compare("father")!=0 && temp.compare("mother")!=0 && temp.compare("grandmother")!=0 && temp.compare("grandfather")!=0 && temp.compare("great")!=0){
-                    cout<<"exception: temp.compare(me)!=0 || temp.compare(father)!=0 || temp.compare(mother)!=0 || temp.compare(grandmother)!=0 || temp.compare(grandfather)!=0 || temp.compare(great)!=0"<<endl;
+                    // cout<<"exception: temp.compare(me)!=0 || temp.compare(father)!=0 || temp.compare(mother)!=0 || temp.compare(grandmother)!=0 || temp.compare(grandfather)!=0 || temp.compare(great)!=0"<<endl;
                     string ex="The tree cannot handle the '";
                     ex.append(kirva);
                     ex.append("' relation");
--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CXX=clang++-9
 CXXFLAGS=-std=c++2a
 
 HEADERS := $(wildcard *.h*)
-TEACHER_SOURCES := Demo.cpp TestCounter.cpp Test.cpp
+TEACHER_SOURCES := Demo.cpp TestCounter.cpp TestAriel.cpp
 STUDENT_SOURCES := $(filter-out $(TEACHER_SOURCES), $(wildcard *.cpp))
 STUDENT_OBJECTS := $(subst .cpp,.o,$(STUDENT_SOURCES))
 
